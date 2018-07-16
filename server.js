94eb3c02c7cbe9b1f89aa57ae7a65aaa616e66c0
unknown <mrhdigital@yahoo.com>
2018-07-16 14:42:44 -0500

    handlebars view

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Require dependencies
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
 // set up our port to be either the host's designated port or 3000
 var PORT = process.env.PORT || 8080;
@@ -12,6 +14,21 @@ var router = express.Router();
 
 // Designate public folder as a static directory
 app.use(express.static(__dirname + "/public"));
+
+// Connect Handlebars to our Express app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+// use bodyParser in our app
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // Have every request go through router middleware
 app.use(router);
